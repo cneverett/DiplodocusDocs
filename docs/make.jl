e6a20c805c37078d4217c30b50473d414ee5df80
Christopher Everett <cn.everett@outlook.com>
2024-10-08 17:02:17 +0100

    stable -> first attempt at multidocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,54 @@
 push!(LOAD_PATH,"../src/")
+
+clonedir = ("--temp" in ARGS) ? mktempdir() : joinpath(@__DIR__, "clones")
+outpath =  ("--temp" in ARGS) ? mktempdir() : joinpath(@__DIR__, "out")
+
+@info """
+Cloning packages into: $(clonedir)
+Building aggregate site into: $(outpath)
+"""
+
 using Documenter
 using DiplodocusDocs
+using MultiDocumenter
 
+
+# build local docs but don't deploy
 makedocs(
     sitename = "DiplodocusDocs",
     authors = "Christopher Everett",
     modules  = Module[],
+    clean = true,
+    doctest = false,
     pages=["Home" => "index.md"],
     checkdocs = :export
+)
+
+# build MultiDocs - one for each package
+docs = [
+    MultiDocumenter.MultiDocRef(
+        upstream = joinpath(@__DIR__,"build"),
+        path = "diplodocusdocs",
+        name = "Home",
+        fix_canonical_url = false,
+    ),
+    MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "BoltzmannCollisionIntegral.jl"),
+        path = "boltzmanncollisionintegrale",
+        name = "BoltzmannCollisionIntegral.jl",
+        giturl = "https://github.com/cneverett/BoltzmannCollisionIntegral.jl.git",
+    )
+]
+
+# build docs
+MultiDocumenter.make(
+    outpath,
+    docs;
+    assets_dir = "docs/src/assets",
+    search_engine = MultiDocumenter.SearchConfig(
+        index_versions = ["stable"],
+        engine = MultiDocumenter.FlexSearch
+    ),
 )
 
 
