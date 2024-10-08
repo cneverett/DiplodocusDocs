f04da811c0377ea11319e5ac1b6e425275cfc2c0
Christopher Everett <cn.everett@outlook.com>
2024-10-08 17:04:25 +0100

    2nd attempt at multidocs

--- docs/make.jl
+++ docs/make.jl
@@ -52,11 +52,48 @@ MultiDocumenter.make(
 )
 
 
-deploydocs(
+#= deploydocs(
     repo="github.com/cneverett/DiplodocusDocs",    
     target = "build",
     branch = "gh-pages",
     devbranch = "main"
     #devurl = "dev",
     #versions = ["stable" => "v^", "v#.#"]
-)
+) =#
+
+if "deploy" in ARGS
+    @warn "Deploying to GitHub" ARGS
+    gitroot = normpath(joinpath(@__DIR__, ".."))
+    run(`git pull`)
+    outbranch = "gh-pages"
+    has_outbranch = true
+    if !success(`git checkout $outbranch`)
+        has_outbranch = false
+        if !success(`git switch --orphan $outbranch`)
+            @error "Cannot create new orphaned branch $outbranch."
+            exit(1)
+        end
+    end
+    for file in readdir(gitroot; join = true)
+        endswith(file, ".git") && continue
+        rm(file; force = true, recursive = true)
+    end
+    for file in readdir(outpath)
+        cp(joinpath(outpath, file), joinpath(gitroot, file))
+    end
+    run(`git add .`)
+    if success(`git commit -m 'Aggregate documentation'`)
+        @info "Pushing updated documentation."
+        if has_outbranch
+            run(`git push`)
+        else
+            run(`git push -u origin $outbranch`)
+        end
+        run(`git checkout main`)
+    else
+        @info "No changes to aggregated documentation."
+    end
+else
+    @info "Skipping deployment, 'deploy' not passed. Generated files in docs/out." ARGS
+    cp(outpath, joinpath(@__DIR__, "out"), force = true)
+end
